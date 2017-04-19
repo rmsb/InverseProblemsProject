29eb1998ff0f23c8938c4e4425ce0e5c9cc3a046
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-04-19 09:24:49 +0300

    Updated main script to create the initial guess reconstruction.

---
+++ prototype/ellipticCut.m
@@ -0,0 +1,16 @@
+% ellipticCut2.m
+
+function filtered = ellipticCut(recon, centerPoint, majorAxis, minorAxis) 
+
+% Image side length
+N = size(recon, 1);
+
+% Blah blah
+[x, y] = meshgrid(linspace(0, 1, N), linspace(0, 1, N));
+
+% Set points outside ellipse zero
+filtered = recon;
+filtered((x - centerPoint(1)).^2 / majorAxis^2 + ...
+         (y - centerPoint(2)).^2 / minorAxis^2 > 1) = 0;
+
+end
--- prototype/main.m
+++ prototype/main.m
@@ -6,15 +6,27 @@
 % - Computes the reconstructions with various alpha.
 % - Produces the L-curve. 
 
+%% Load sinogram. 
+load data/LotusSinogram sinogram
+
 %% Generate system matrix. 
 sysmat = SystemMatrix(128, 20, 16);
 save('data/SystemMatrix','sysmat');
 
 %% Sparsify the sinogram. 
+load data/SystemMatrix sysmat
 [sparseSino, sparseRecon] = createSparseSinogram(sinogram, sysmat);
- 
-%% Construct median-filtered target reconstruction. 
-% TO-DO
+save('data/SparseData', 'sparseSino', 'sparseRecon'); 
+
+%% Construct filtered target reconstruction. 
+load data/SparseData sparseRecon
+filteredRecon = ellipticCut(sparseRecon, [0.5, 0.51], 0.37, 0.27);
+filteredRecon = filteredImage(filteredRecon, 3, 0.1);
+save('data/InitialGuess', 'filteredRecon');
+
+%% View filtered target reconstruction. 
+load data/InitialGuess filteredRecon
+imagesc(filteredRecon);
 
 %% Compute reconstructions with various values of alpha.
 % TO-DO
