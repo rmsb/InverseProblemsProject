a2329039bdb02582966ce3952b9828facf74ec66
rmsb <rasmus.backholm@helsinki.fi>
2017-04-25 14:29:00 +0300

    Add files via upload

--- plotLCurves.m
+++ plotLCurves.m
@@ -11,6 +11,7 @@ LCurveX = {filterCurve.LCurveX; zeroCurve.LCurveX};
 LCurveY = {filterCurve.LCurveY; zeroCurve.LCurveY};
 CornerX = {filterCorner.LCurveX; zeroCorner.LCurveX};
 CornerY = {filterCorner.LCurveY; zeroCorner.LCurveY};
+alpha = {filterCorner.alpha; zeroCorner.alpha};
 filenames = {'LCurveFilteredPrior', 'LCurveZeroPrior'};
 
 %% Make plots. 
@@ -20,11 +21,12 @@ for i = 1:size(LCurveX, 1)
     plot(LCurveX{i}, LCurveY{i}, 'LineWidth', 5);
     hold on; 
     plot(CornerX{i}, CornerY{i}, 'r.', 'MarkerSize', 40);
+    text(CornerX{i}, CornerY{i}, ['$\quad\:\alpha=', num2str(alpha{i}, 2), '$'], 'Interpreter', 'latex') 
     axis square;
-    xticks([]);
-    yticks([]);
-    % xlabel('ln || Af - m ||');
-    % ylabel('ln || f - f^* ||');
+    %xticks([]);
+    %yticks([]);
+    xlabel('$\log\left\Vert A\mathbf{f}-\mathbf{m}\right\Vert _2$', 'Interpreter', 'latex');
+    ylabel('$\log\left\Vert \mathbf{f}-\mathbf{f}^\star\right\Vert _2$', 'Interpreter', 'latex');
     filepath = strjoin({'images/', filenames{i}}, '');
     saveas(gcf, filepath, 'epsc');
 end
